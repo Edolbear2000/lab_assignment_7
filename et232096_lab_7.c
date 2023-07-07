307922690130af2f9b4db58b61ced0a30b5ce3ed
Edolbear2000 <61603056+Edolbear2000@users.noreply.github.com>
2023-07-07 16:29:20 -0400

    Add files via upload

--- et232096_lab_7.c
+++ et232096_lab_7.c
@@ -1,36 +1,45 @@
 #include <stdio.h>
 
-void bubbleSort(int arr[], int swap_count[], int n) {
-    for(int i=0; i<n-1; i++) {
-        for(int j=0; j<n-i-1; j++) {
-            if(arr[j] > arr[j+1]) {
-                swap_count[j]++;
-                swap_count[j+1]++;
+// Function to swap the values at two indices in the array and update the count of swaps for the index being swapped
+void swap(int* a, int* b, int* swaps_a) {
+    int t = *a;
+    *a = *b;
+    *b = t;
+    (*swaps_a)++;
+}
 
-                int temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
+// Modified Bubble Sort function that keeps track of how many times a value at each index gets swapped
+void bubbleSort(int arr[], int n, int swaps[]) {
+    int i, j;
+    for (i = 0; i < n-1; i++) {       
+        for (j = 0; j < n-i-1; j++) {
+            if (arr[j] > arr[j+1]) {
+                swap(&arr[j], &arr[j+1], &swaps[j]);
             }
         }
     }
 }
 
-void printSwapCounts(int swap_count[], int n) {
-    for(int i=0; i<n; i++) {
-        printf("Index %d: %d swaps\n", i, swap_count[i]);
-    }
-}
-
 int main() {
     int arr[] = {97, 16, 45, 63, 13, 22, 7, 58, 72};
-    int n = sizeof(arr)/sizeof(arr[0]);
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int swaps[n];
     
-    int swap_count[n];
-    for(int i=0; i<n; i++)
-        swap_count[i] = 0;
+    // Initialize the swaps array with zeros
+    for(int i = 0; i < n; i++) {
+        swaps[i] = 0;
+    }
 
-    bubbleSort(arr, swap_count, n);
-    printSwapCounts(swap_count, n);
+    // Bubble sort the array while counting the swaps
+    bubbleSort(arr, n, swaps);
+
+    // Print out the swaps for each index
+    int totalSwaps = 0;
+    for(int i = 0; i < n; i++) {
+        printf("Number of swaps needed for index %d: %d\n", i, swaps[i]);
+        totalSwaps += swaps[i];
+    }
+    printf("Total number of swaps: %d\n", totalSwaps);
 
     return 0;
 }
